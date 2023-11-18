e4aac8f66312ae4a5d15445afc2bba8c2b8791ed
Ernest Law <elawvc@gmail.com>
2023-11-18 13:54:08 -0800

    2 - Implemented Create Function in reminder_controller.js - issue with completed tab however

--- controller/reminder_controller.js
+++ controller/reminder_controller.js
@@ -44,6 +44,21 @@ let remindersController = {
   // Goes into the database and updates the reminder within the database
   update: (req, res) => {
     // implementation here 👈
+    console.log(req.body);
+    // Find the reminder that has the same title
+    let reminderToFind = req.params.id; 
+    let reminderIndex = database.cindy.reminders.findIndex(function (reminder) {
+      return reminder.id == reminderToFind; 
+    })   
+    if (reminderIndex !== -1) {
+      // Update the reminder with the new title and description
+      database.cindy.reminders[reminderIndex].title = req.body.title; 
+      database.cindy.reminders[reminderIndex].description = req.body.description; 
+      database.cindy.reminders[reminderIndex].completed = req.body.completed === "on";
+      console.log(req.body.completed)
+      // Go back to the reminders page
+      res.redirect("/reminders");
+    }
   },
 
   // Just deletes the reminder that is listed 
